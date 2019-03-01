28e1762ea6dce2bd5dc25c3720121ca3b680c61e
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-01 05:13:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,8 @@ end
 def selective_tweet_shortener tweet
   tweet.length > 140 ? word_substituter(tweet) : tweet
 end
+
+def shortened_tweet_truncator tweet
+  shortened_tweet = tweet.length > 140 ? word_substituter(tweet) : tweet
+  shortened_tweet.length > 140 ? "#{shortened_tweet[0..136]}..." : shortened_tweet
+end
